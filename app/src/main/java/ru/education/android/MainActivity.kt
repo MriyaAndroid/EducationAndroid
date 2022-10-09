ad2d477bbaa58f7ab044c0228a9c632b511d42cb
Sergei <sergei.pokhodai@gmail.com>
2022-10-09 15:41:28 +0400

    lesson_1

--- app/src/main/java/ru/education/android/MainActivity.kt
+++ app/src/main/java/ru/education/android/MainActivity.kt
@@ -2,10 +2,34 @@ package ru.education.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
+// : -> наследование
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            textView.text = "Russia"
+        }
     }
+
+    //Run
+    //I/System.out: onStart
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+    }
+
+    //I/System.out: onStop
+    override fun onStop() {
+        super.onStop()
+        println("onStop")
+    }
+
 }
