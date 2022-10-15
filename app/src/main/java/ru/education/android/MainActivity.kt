34cd8f0de1b3f5cbb95b6c8871402ec8ec2468cc
Lykov Alexander <ordya2@mail.ru>
2022-10-15 23:36:13 +0400

    test

--- app/src/main/java/ru/education/android/MainActivity.kt
+++ app/src/main/java/ru/education/android/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.education.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.makeText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastME (view: View) {
+        val myToast = Toast.makeText(this, "You are very Hard GAY", Toast.LENGTH_SHORT)
+
+            myToast.show()
+    }
+    fun cauntME (view: View) {
+
     }
 }
