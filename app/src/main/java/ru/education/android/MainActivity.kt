5c768b2061655edaad896807f0f480cb65a10ae2
Sergei <sergei.pokhodai@gmail.com>
2022-10-16 13:56:14 +0300

    commit anyWay

--- app/src/main/java/ru/education/android/MainActivity.kt
+++ app/src/main/java/ru/education/android/MainActivity.kt
@@ -9,17 +9,25 @@ import android.widget.Toast
 import android.widget.Toast.makeText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun toastME (view: View) {
-        val myToast = Toast.makeText(this, "You are very Hard GAY", Toast.LENGTH_SHORT)
-
-            myToast.show()
-    }
-    fun cauntME (view: View) {
-
+        val youButton = findViewById<Button>(R.id.YouButton)
+        val wEButton = findViewById<Button>(R.id.WEButton)
+        val aSSButton = findViewById<Button>(R.id.ASSButton)
+        val serGAY = findViewById<TextView>(R.id.SerGAY)
+        youButton.setOnClickListener {
+            serGAY.text = "You are very Hard GAY"
+        }
+        wEButton.setOnClickListener {
+            serGAY.text = "You are very Very VERY HARD GAY"
+        }
+        aSSButton.setOnClickListener {
+            serGAY.text = "You are very Very VERY VERY HARD GAY"
+        }
+        serGAY.setOnClickListener {
+            serGAY.text = "NE GEY"
+        }
     }
 }
